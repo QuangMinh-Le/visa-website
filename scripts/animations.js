b1fea94c32630dff02484e855dacf2c60826f85c
quangminh <quangminh.ottawa@gmail.com>
2025-02-16 22:59:29 -0500

    add register page and buy merch page

---
+++ scripts/animations.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+   const sections = document.querySelectorAll(".info-section");
+
+   const observer = new IntersectionObserver((entries, observer) => {
+       entries.forEach(entry => {
+           if (entry.isIntersecting) {
+               entry.target.classList.add("visible");
+               observer.unobserve(entry.target);
+           }
+       });
+   }, { threshold: 0.3 });
+
+   sections.forEach(section => {
+       observer.observe(section);
+   });
+});
